4bef7a885c02458c247c077e7db80c65f2483b35
Kles <klesta.luli@gmail.com>
2022-06-10 13:48:00 +0400

    added new domain

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React, { Fragment } from "react"
-import Dictionary from "./Components/Dictionary/Dictionary"
-import Header from "./Components/UI/Header"
+import React, { Fragment } from "react";
+import Dictionary from "./Components/Dictionary/Dictionary";
+import Header from "./Components/UI/Header";
 
 export default function App() {
-    return (
-        <Fragment>
-            <Header />
-            <Dictionary defaultWord='wine' />
-            <footer>Coded by Klesta | <a
-                href="https://github.com/klezi10/react-my-dictionary-app"
-                target="_blank"
-                rel="noopener noreferrer"
-            >Open-source</a> </footer>
-        </Fragment>
-    )
+  return (
+    <Fragment>
+      <Header />
+      <Dictionary defaultWord="wine" />
+      <footer>
+        Coded by{" "}
+        <a
+          href="https://www.klesdev.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          Klesta Luli
+        </a>{" "}
+        |{" "}
+        <a
+          href="https://github.com/klezi10/react-my-dictionary-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Open-source
+        </a>{" "}
+      </footer>
+    </Fragment>
+  );
 }
